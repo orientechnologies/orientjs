de99a58c479e115fd95b06a570d8eeeef4ed2c2a
Gabriel Petrovay <gabipetrovay@gmail.com>
2012-02-28 18:27:55 +0100

    Added all DB commands. The binary protocol is not sound! I have to report some bugs: - why DB_DELETE returns in the spec a byte? isn't the status enough? - why DB_DELETE in practive does not return the byte as specified? - mismatch between DB_RELOAD and DB_OPEN (types are different and missing bluster type in one) - DB_SIZE after DB_CREATE generates a problem (NullPointerException), nothing comes in the DB_SIZE response and something comes through the socket a little later when it is too late.
    Added two examples

---
+++ examples/connect_create_drop.js
@@ -0,0 +1,41 @@
+var Db = require('../lib/orientdb').Db,
+    Connection = require('../lib/orientdb').Connection,
+    Server = require('../lib/orientdb').Server;
+
+
+var dbConfig = {
+    user_name: "admin",
+    user_password: "admin",
+};
+var serverConfig = {
+    host: 'localhost',
+    port: Connection.DEFAULT_PORT,
+    user_name: "root",
+    user_password: "83CACE21A23DB46F93BFD58A3CE48C8D29926C6EF424D7DA9BD725AE070CCDC0"
+};
+
+var server = new Server(serverConfig);
+var db = new Db('test', server, dbConfig);
+
+
+server.connect(function(err, sessionId) {
+
+    if (err) { console.log(err); return; }
+
+    console.log("Connected on session: " + sessionId);
+
+    db.create(function(err) {
+        
+        if (err) { console.log(err); return; }
+    
+        console.log("Created database: " + db.databaseName);
+
+        db.delete(function(err) {
+
+            if (err) { console.log(err); return; }
+
+            console.log("Deleted database");
+        });
+    });
+});
+
---
+++ examples/open_size_close.js
@@ -0,0 +1,35 @@
+var Db = require('../lib/orientdb').Db,
+    Connection = require('../lib/orientdb').Connection,
+    Server = require('../lib/orientdb').Server;
+
+
+var dbConfig = {
+    user_name: "admin",
+    user_password: "admin",
+};
+var serverConfig = {
+    host: 'localhost',
+    port: Connection.DEFAULT_PORT,
+    user_name: "root",
+    user_password: "83CACE21A23DB46F93BFD58A3CE48C8D29926C6EF424D7DA9BD725AE070CCDC0"
+};
+
+var server = new Server(serverConfig);
+var db = new Db('temp', server, dbConfig);
+
+
+db.open(function(err, result) {
+
+    if (err) { console.log(err); return; }
+
+    console.log("Opened database on session: " + result.sessionId);
+    console.log("Database '" + db.databaseName + "' has " + result.clusters.length + " clusters");
+
+    db.size(function(err, size) {
+
+        if (err) { console.log(err); return; }
+
+        console.log("Database size: " + size);
+    });
+});
+
