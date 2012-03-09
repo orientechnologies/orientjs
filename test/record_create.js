c95f7a258159c464720f5d2f7d29356069e75215
Federico Fissore <federico@fsfe.org>
2012-03-09 18:13:47 +0100

    record create

---
+++ test/record_create.js
@@ -0,0 +1,39 @@
+var Db = require("../lib/orientdb").Db,
+    Server = require("../lib/orientdb").Server
+    assert = require("assert");
+
+var serverConfig = require("../config/test/serverConfig");
+var dbConfig = require("../config/test/dbConfig");
+
+var server = new Server(serverConfig);
+var db = new Db("temp", server, dbConfig);
+
+
+db.open(function(err, result) {
+
+    if (err) { console.log(err); return; }
+
+    var data = new Buffer(14);
+    data.write("this is a test");
+    
+    var recordData = {
+      clusterId: result.clusters[0].id,
+      content: data,
+      recordType: "b"
+    }
+    
+    db.createRecord(recordData, function(err, result) {
+        
+        var firstRecord = result.position;
+
+        db.createRecord(recordData, function(err, result) {
+            
+            assert(result.position == (firstRecord + 1));
+
+            db.close();
+        });
+        
+    });
+    
+});
+
