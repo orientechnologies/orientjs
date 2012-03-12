25614ac4846f38f977a330badfb2876c2fd98e0a
Federico Fissore <federico@fsfe.org>
2012-03-12 16:05:30 +0100

    record create

--- test/record_create_document.js
+++ test/record_create_document.js
@@ -13,18 +13,25 @@ db.open(function(err, result) {
 
     if (err) { console.log(err); return; }
 
-    var data = new Buffer(14);
-    data.write("this is a test");
+    var first_doc_data = "TestClass@nick:\"ThePresident\",subdoc:(name:\"subdoc name\",id_field:42),follows:[],followers:[],name:\"Barack\",surname:\"Obama\",location:#3:2,invitedBy:,salary_cloned:,salary:120.3f";
+    var data = new Buffer(first_doc_data.length);
+    data.write(first_doc_data);
     
+    var clusterId = result.clusters[0].id
     var recordData = {
-      clusterId: result.clusters[0].id,
+      clusterId: clusterId,
       content: data,
-      recordType: "b"
+      recordType: "d"
     }
     
     db.createRecord(recordData, function(err, result) {
         
         var firstRecord = result.position;
+        
+        var second_doc_data = first_doc_data.replace("followers:[]", "followers:[#" + clusterId + ":" + firstRecord + "]");
+        var data = new Buffer(second_doc_data.length);
+        data.write(second_doc_data);
+        recordData.content = data;
 
         db.createRecord(recordData, function(err, result) {
             
