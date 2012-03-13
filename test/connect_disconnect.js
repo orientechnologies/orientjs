4d2cf2b11a06e0fa20142939893fc0a414a698fa
Gabriel Petrovay <gabipetrovay@gmail.com>
2012-03-13 15:18:43 +0100

    Removed two forgotten "debugger" instructions

--- test/connect_disconnect.js
+++ test/connect_disconnect.js
@@ -6,15 +6,15 @@ var server = new Server(serverConfig);
 
 
 server.connect(function(err, sessionId) {
-debugger;
+
     if (err) { console.log(err); return; }
 
     console.log('Connected on session: ' + sessionId);
 
     server.disconnect(function(err) {
-        
+
         if (err) { console.log(err); return; }
-    
+
         console.log('Closed connection');
     });
 });
