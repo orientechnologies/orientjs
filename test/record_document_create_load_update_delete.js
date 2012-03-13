4b2ca88ff24e33b0db41f83fb192b1f0fe765b7c
Federico Fissore <federico@fsfe.org>
2012-03-13 16:03:05 +0100

    record delete and update

--- test/record_document_create_load_update_delete.js
+++ test/record_document_create_load_update_delete.js
@@ -19,22 +19,57 @@ db.open(function(err, result) {
     
     var clusterId = result.clusters[0].id
     var recordData = {
-      clusterId: clusterId,
-      content: data,
-      recordType: "d"
+        clusterId: clusterId,
+        content: data,
+        recordType: "d"
     }
     
     db.createRecord(recordData, function(err, result) {
-
-        var recordData = {
-          clusterId: clusterId,
-          clusterPosition: result.position
+        
+        var createdRecord = {
+            clusterId: clusterId,
+            clusterPosition: result.position
         }
         
-        var record = db.loadRecord(recordData, function(err, result) {
+        db.loadRecord(createdRecord, function(err, result) {
+            var first_version = result.version;
+            
             assert.equal(first_doc_data.substr(10), result.content.toString().trim());
             
-            db.close();
+            var second_doc_data = "TestClass@nick:\"TheVicePresident\",subdoc:(name:\"subdoc name\",id_field:42),follows:[],followers:[],name:\"Joe\",surname:\"Biden\",location:#3:2,invitedBy:,salary_cloned:,salary:120.3f";
+            var data = new Buffer(second_doc_data.length);
+            data.write(second_doc_data);
+
+            var updateRecordPreviousVersion = {
+                clusterId: createdRecord.clusterId,
+                clusterPosition: createdRecord.clusterPosition,
+                content: data,
+                recordType: "d",
+                version: first_version
+            }
+            
+            db.updateRecord(updateRecordPreviousVersion, function(err, result) {
+                assert.equal(result.version, updateRecordPreviousVersion.version + 1);
+                
+                var current_version = result.version;
+                
+                db.updateRecord(updateRecordPreviousVersion, function(err, result) {
+                    assert(err != null);
+                    
+                    var deleteRecord = {
+                        clusterId: updateRecordPreviousVersion.clusterId,
+                        clusterPosition: updateRecordPreviousVersion.clusterPosition,
+                        version: current_version
+                    }
+                    
+                    db.deleteRecord(deleteRecord, function(err, result) {
+                        assert.equal(1, result.status);
+                        
+                        db.close();
+                    });
+                });
+            });
+            
         });
         
     });
