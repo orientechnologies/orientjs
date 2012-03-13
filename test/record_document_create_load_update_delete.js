e90827b572e2e6c2047fb03c899e4d7250240862
Federico Fissore <federico@fsfe.org>
2012-03-13 15:20:03 +0100

    record load

---
+++ test/record_document_create_load_update_delete.js
@@ -0,0 +1,43 @@
+var Db = require("../lib/orientdb").Db,
+    Server = require("../lib/orientdb").Server
+    assert = require("assert");
+
+var serverConfig = require("../config/test/serverConfig");
+var dbConfig = require("../config/test/dbConfig");
+
+var server = new Server(serverConfig);
+var db = new Db("temp", server, dbConfig);
+
+
+db.open(function(err, result) {
+
+    if (err) { console.log(err); return; }
+
+    var first_doc_data = "TestClass@nick:\"ThePresident\",subdoc:(name:\"subdoc name\",id_field:42),follows:[],followers:[],name:\"Barack\",surname:\"Obama\",location:#3:2,invitedBy:,salary_cloned:,salary:120.3f";
+    var data = new Buffer(first_doc_data.length);
+    data.write(first_doc_data);
+    
+    var clusterId = result.clusters[0].id
+    var recordData = {
+      clusterId: clusterId,
+      content: data,
+      recordType: "d"
+    }
+    
+    db.createRecord(recordData, function(err, result) {
+
+        var recordData = {
+          clusterId: clusterId,
+          clusterPosition: result.position
+        }
+        
+        var record = db.loadRecord(recordData, function(err, result) {
+            assert.equal(first_doc_data.substr(10), result.content.toString().trim());
+            
+            db.close();
+        });
+        
+    });
+    
+});
+
