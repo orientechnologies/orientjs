453d28d6b93085c4f0193d11741f08dbf6186876
Federico Fissore <federico@fsfe.org>
2012-03-14 16:08:36 +0100

    added create/drop cluster/class in the create/drop document test

--- test/record_document_create_load_update_delete.js
+++ test/record_document_create_load_update_delete.js
@@ -11,64 +11,90 @@ var db = new Db("temp", server, dbConfig);
 
 
 db.open(function(err, result) {
-
-    assert(!err, "Error while opening the database: " + err);
-
-    var first_doc_data = "TestClass@nick:\"ThePresident\",subdoc:(name:\"subdoc name\",id_field:42),follows:[],followers:[],name:\"Barack\",surname:\"Obama\",location:#3:2,invitedBy:,salary_cloned:,salary:120.3f";
-    var data = new Buffer(first_doc_data.length);
-    data.write(first_doc_data);
     
-    var clusterId = result.clusters[0].id
-    var recordData = {
-        clusterId: clusterId,
-        content: data,
-        recordType: "d"
+    assert(!err, "Error while opening the database: " + err);
+    
+    var clusterOptions = {
+        type: "MEMORY",
+        name: "TestClass"
     }
     
-    db.createRecord(recordData, function(err, result) {
-        
-        var createdRecord = {
-            clusterId: clusterId,
-            clusterPosition: result.position
-        }
-        
-        db.loadRecord(createdRecord, function(err, result) {
-            var first_version = result.version;
-            
-            assert.equal(first_doc_data.substr(10), result.content.toString().trim());
-            
-            var second_doc_data = "TestClass@nick:\"TheVicePresident\",subdoc:(name:\"subdoc name\",id_field:42),follows:[],followers:[],name:\"Joe\",surname:\"Biden\",location:#3:2,invitedBy:,salary_cloned:,salary:120.3f";
-            var data = new Buffer(second_doc_data.length);
-            data.write(second_doc_data);
+    assert.equal(5, db.clusters.length);
 
-            var updateRecordPreviousVersion = {
-                clusterId: createdRecord.clusterId,
-                clusterPosition: createdRecord.clusterPosition,
-                content: data,
-                recordType: "d",
-                version: first_version
-            }
+    db.addDataCluster(clusterOptions, function(err, clusterId) {
+        
+        db.reload(function(err, result) {
             
-            db.updateRecord(updateRecordPreviousVersion, function(err, result) {
-                assert.equal(result.version, updateRecordPreviousVersion.version + 1);
+            assert.equal(6, db.clusters.length);
+            
+            db.command("create class TestClass cluster " + db.clusters[5].id, function(err, result) {
+        
+                var first_doc_data = "TestClass@nick:\"ThePresident\",subdoc:(name:\"subdoc name\",id_field:42),follows:[],followers:[],name:\"Barack\",surname:\"Obama\",location:#3:2,invitedBy:,salary_cloned:,salary:120.3f";
+                var data = new Buffer(first_doc_data.length);
+                data.write(first_doc_data);
                 
-                var current_version = result.version;
+                var recordData = {
+                    clusterId: clusterId,
+                    content: data,
+                    recordType: "d"
+                }
                 
-                db.updateRecord(updateRecordPreviousVersion, function(err, result) {
-                    assert(err != null);
+                db.createRecord(recordData, function(err, result) {
                     
-                    var deleteRecord = {
-                        clusterId: updateRecordPreviousVersion.clusterId,
-                        clusterPosition: updateRecordPreviousVersion.clusterPosition,
-                        version: current_version
+                    var createdRecord = {
+                        clusterId: clusterId,
+                        clusterPosition: result.position
                     }
                     
-                    db.deleteRecord(deleteRecord, function(err, result) {
-                        assert.equal(1, result.status);
+                    db.loadRecord(createdRecord, function(err, result) {
+                        var first_version = result.version;
                         
-                        db.close();
+                        assert.equal(first_doc_data, result.content.toString().trim());
+                        
+                        var second_doc_data = "TestClass@nick:\"TheVicePresident\",subdoc:(name:\"subdoc name\",id_field:42),follows:[],followers:[],name:\"Joe\",surname:\"Biden\",location:#3:2,invitedBy:,salary_cloned:,salary:120.3f";
+                        var data = new Buffer(second_doc_data.length);
+                        data.write(second_doc_data);
+            
+                        var updateRecordPreviousVersion = {
+                            clusterId: createdRecord.clusterId,
+                            clusterPosition: createdRecord.clusterPosition,
+                            content: data,
+                            recordType: "d",
+                            version: first_version
+                        }
+                        
+                        db.updateRecord(updateRecordPreviousVersion, function(err, result) {
+                            assert.equal(result.version, updateRecordPreviousVersion.version + 1);
+                            
+                            var current_version = result.version;
+                            
+                            db.updateRecord(updateRecordPreviousVersion, function(err, result) {
+                                assert(err != null);
+                                
+                                var deleteRecord = {
+                                    clusterId: updateRecordPreviousVersion.clusterId,
+                                    clusterPosition: updateRecordPreviousVersion.clusterPosition,
+                                    version: current_version
+                                }
+                                
+                                db.deleteRecord(deleteRecord, function(err, result) {
+                                    assert.equal(1, result.status);
+                                    
+                                    db.command("drop class TestClass", function(err, result) {
+                                        assert(result);
+                                        db.close();
+                                    });
+                                    
+                                });
+                                
+                            });
+                            
+                        });
+                        
                     });
+                    
                 });
+                
             });
             
         });
