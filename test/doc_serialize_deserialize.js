c9194c447a654de014ebb600b71c650a5f53894e
Federico Fissore <federico@fsfe.org>
2012-03-16 18:13:30 +0100

    serialize/deserialize done. back to save document

--- test/doc_serialize_deserialize.js
+++ test/doc_serialize_deserialize.js
@@ -4,23 +4,19 @@ var parser = require("../lib/orientdb/connection/parser");
 var document = {
   _class: "FantasyPerson",
   name: "it's me, \\ \"",
-  birthday: new Date(),
+  birthday: new Date(Math.floor(new Date().getTime() / 1000) * 1000),
   fingers: 20,
   a_float: 123.4,
   like_it: true,
   linked_to: "#1:1",
-  last_time_in: { name: "Turin, Italy", when: new Date() },
+  last_time_in: { name: "Turin, Italy", when: new Date(Math.floor(new Date().getTime() / 1000) * 1000) },
   cities: [{ name: "Turin, Italy" }],
   known_os_list: [ "linux" ],
   zero_is: null
 }
 
-var serialized_document = "FantasyPerson@name:\"it's me, \\\\ \\\"\",birthday:" + Math.floor(document.birthday.getTime() / 1000) + "t,fingers:20,a_float:123.4f,like_it:true,linked_to:#1:1,last_time_in:(name:\"Turin, Italy\",when:" + Math.floor(document.last_time_in.when.getTime() / 1000) + "t),cities:(name:\"Turin, Italy\")known_os_list:[\"linux\"],zero_is:";
-
-console.log(document);
-console.log(parser.serializeDocument(document));
-console.log(serialized_document);
-console.log(parser.deserializeDocument(parser.serializeDocument(document)));
+var serialized_document = "FantasyPerson@name:\"it's me, \\\\ \\\"\",birthday:" + Math.floor(document.birthday.getTime() / 1000) + "t,fingers:20,a_float:123.4f,like_it:true,linked_to:#1:1,last_time_in:(name:\"Turin, Italy\",when:" + Math.floor(document.last_time_in.when.getTime() / 1000) + "t),cities:[(name:\"Turin, Italy\")],known_os_list:[\"linux\"],zero_is:";
 
 assert.equal(serialized_document, parser.serializeDocument(document));
-assert.equal(document, parser.deserializeDocument(serialized_document));
+assert.equal(JSON.stringify(document), JSON.stringify(parser.deserializeDocument(serialized_document)));
+
