8c8bb818a624f4902bff4627bb121302f9a13bff
Federico Fissore <federico@fsfe.org>
2012-03-19 18:14:49 +0100

    double quotes

--- test/document_save.js
+++ test/document_save.js
@@ -1,13 +1,13 @@
 var assert = require("assert");
 
-var Db = require('../lib/orientdb').Db,
-    Server = require('../lib/orientdb').Server;
+var Db = require("../lib/orientdb").Db,
+    Server = require("../lib/orientdb").Server;
 
-var serverConfig = require('../config/test/serverConfig');
-var dbConfig = require('../config/test/dbConfig');
+var serverConfig = require("../config/test/serverConfig");
+var dbConfig = require("../config/test/dbConfig");
 
 var server = new Server(serverConfig);
-var db = new Db('temp', server, dbConfig);
+var db = new Db("temp", server, dbConfig);
 
 //var binary_data = new Buffer(1);
 //binary_data.writeUInt8(42, 0);
