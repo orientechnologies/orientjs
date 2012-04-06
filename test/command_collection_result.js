4298dd101e39f6a27cb3f4e8588e0621a2389bf4
Federico Fissore <federico@fsfe.org>
2012-04-06 16:59:23 +0200

    closes #19

--- test/command_collection_result.js
+++ test/command_collection_result.js
@@ -10,16 +10,19 @@ var dbConfig = require("../config/test/dbConfig");
 var server = new Server(serverConfig);
 var db = new Db("temp", server, dbConfig);
 
-
 db.open(function(err, result) {
 
     assert(!err, "Error while opening the database: " + err);
 
     db.command("SELECT FROM OUser", function(err, results) {
- 
+
         assert(!err, "Error while executing a SELECT command: " + err);
 
         assert.equal(results.length, 3, "Weren't there 3 users in this database?");
+
+        for (var i in results) {
+            assert.equal(typeof results[i]["@rid"], "string");
+        }
 
         console.log("Received results: " + JSON.stringify(results));
 
