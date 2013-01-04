5f5f6c27793f368fc31837a08849d70155f65f1c
Federico Fissore <federico.fissore@gmail.com>
2013-01-04 21:19:10 +0100

    closes #92

--- test/date.js
+++ test/date.js
@@ -20,20 +20,27 @@ graphdb.open(function(err) {
         graphdb.command("CREATE PROPERTY testSchema.label string", function(err) {
             assert(!err, err);
 
-            graphdb.command("CREATE PROPERTY testSchema.data date", function(err) {
+            graphdb.command("CREATE PROPERTY testSchema.a_date date", function(err) {
                 assert(!err, err);
 
-                graphdb.createVertex({ label: "This date should be 2012-12-21", data: "2012-12-21" }, { class: "testSchema" }, function(err, vertex) {
+                graphdb.command("CREATE PROPERTY testSchema.a_datetime datetime", function(err) {
                     assert(!err, err);
 
-                    assert.equal(2012, vertex.data.getFullYear());
-                    assert.equal(11, vertex.data.getMonth());
-                    assert.equal(21, vertex.data.getDate());
-                    
-                    graphdb.dropClass("testSchema", function(err) {
+                    graphdb.createVertex({ label: "This date should be 2012-12-21", a_date: "2012-12-21", a_datetime: 1296279468123 }, { class: "testSchema" }, function(err, vertex) {
                         assert(!err, err);
 
-                        graphdb.close();
+                        assert.equal(2012, vertex.a_date.getFullYear());
+                        assert.equal(11, vertex.a_date.getMonth());
+                        assert.equal(21, vertex.a_date.getDate());
+                        
+                        assert.equal(0, vertex.a_date.getMilliseconds());
+                        assert.equal(123, vertex.a_datetime.getMilliseconds());
+
+                        graphdb.dropClass("testSchema", function(err) {
+                            assert(!err, err);
+
+                            graphdb.close();
+                        });
                     });
                 });
             });
