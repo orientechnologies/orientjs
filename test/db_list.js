ca265b2f2ee864c7b884fb73d28bbdf5f03e269d
Federico Fissore <federico@fissore.org>
2013-01-18 17:21:28 +0100

    DB_LIST

---
+++ test/db_list.js
@@ -0,0 +1,28 @@
+var assert = require("assert");
+var parser = require("../lib/orientdb/connection/parser");
+var _ = require("underscore");
+
+var orient = require("../lib/orientdb"),
+    Db = orient.Db,
+    Server = orient.Server;
+
+var serverConfig = require("../config/test/serverConfig");
+var dbConfig = require("../config/test/dbConfig");
+
+var server = new Server(serverConfig);
+
+server.connect(function(err) {
+
+    assert(!err, err);
+
+    server.listDatabases(function(err, databases) {
+        assert(!err, err);
+
+        assert.equal(0, databases.temp.indexOf("memory:"));
+        assert.equal(0, databases.tinkerpop.indexOf("local:"));
+
+        server.disconnect();
+    })
+
+});
+
