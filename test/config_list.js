adf3962256687c585cc0faaefba42ca391031ac4
Federico Fissore <federico@fissore.org>
2013-01-18 18:11:16 +0100

    CONFIG_LIST

---
+++ test/config_list.js
@@ -0,0 +1,29 @@
+var assert = require("assert");
+var parser = require("../lib/orientdb/connection/parser");
+var _ = require("underscore");
+
+var orient = require("../lib/orientdb"),
+    Db = orient.Db,
+    Server = orient.Server;
+
+var serverConfig = require("../config/test/serverConfig");
+var dbConfig = require("../config/test/dbConfig");
+
+var server = new Server(serverConfig);
+
+server.connect(function(err) {
+
+    assert(!err, err);
+
+    server.configList(function(err, config) {
+        assert(!err, err);
+
+        assert(!_.isEmpty(config));
+        assert(config["log.file.level"]);
+        assert(config["log.console.level"]);
+
+        server.disconnect();
+    })
+
+});
+
