86183e89deb51f419b3af1f84264c510ed4c675b
Federico Fissore <federico@fissore.org>
2013-02-08 11:25:19 +0100

    working on #108: RIDs are not updated after commit

--- test/transactions.js
+++ test/transactions.js
@@ -11,7 +11,12 @@ var dbConfig = require("../config/test/dbConfig");
 var server = new Server(serverConfig);
 var db = new Db("temp", server, dbConfig);
 
-var userClusterId, linkClusterId, transaction, firstExistingDoc, secondExistingDoc, firstNewDoc, secondNewDoc, newLink;
+var userClusterId, linkClusterId, transaction,
+    firstExistingDoc, firstExistingDocRID, firstExistingDocVersion,
+    secondExistingDoc, secondExistingDocRID,
+    firstNewDoc, firstNewDocRID,
+    secondNewDoc, secondNewDocRID,
+    newLink, newLinkRID;
 
 async.waterfall([
     function(callback) {
@@ -37,6 +42,8 @@ async.waterfall([
     },
     function(_firstExistingDoc, callback) {
         firstExistingDoc = _firstExistingDoc;
+        firstExistingDocRID = _firstExistingDoc["@rid"];
+        firstExistingDocVersion = _firstExistingDoc["@version"];
 
         assert.equal(0, firstExistingDoc["@version"]);
 
@@ -53,6 +60,7 @@ async.waterfall([
     },
     function(_secondExistingDoc, callback) {
         secondExistingDoc = _secondExistingDoc;
+        secondExistingDocRID = _secondExistingDoc["@rid"];
 
         assert.equal(0, secondExistingDoc["@version"]);
 
@@ -85,6 +93,7 @@ async.waterfall([
     },
     function(_firstNewDoc, callback) {
         firstNewDoc = _firstNewDoc;
+        firstNewDocRID = _firstNewDoc["@rid"];
 
         assert.equal(-2, transaction.clusterPosition);
         assert.equal(1, transaction.docs.length);
@@ -98,6 +107,7 @@ async.waterfall([
     },
     function(_secondNewDoc, callback) {
         secondNewDoc = _secondNewDoc;
+        secondNewDocRID = _secondNewDoc["@rid"];
 
         assert.equal(-3, transaction.clusterPosition);
         assert.equal(2, transaction.docs.length);
@@ -112,6 +122,7 @@ async.waterfall([
     },
     function(_newLink, callback) {
         newLink = _newLink;
+        newLinkRID = _newLink["@rid"];
 
         callback();
     },
@@ -159,6 +170,15 @@ async.waterfall([
         assert.equal(userClusterId, result.recordsUpdated[0].clusterId);
         assert.equal(0, result.recordsUpdated[0].clusterPosition);
         assert.equal(1, result.recordsUpdated[0].version);
+        
+        assert.equal(firstExistingDoc["@rid"], firstExistingDocRID);
+        assert.equal(firstExistingDoc["@version"], firstExistingDocVersion + 1);
+        assert(firstNewDoc["@rid"] !== firstNewDocRID);
+        assert.equal(firstNewDoc["@version"], 0);
+        assert(secondNewDoc["@rid"] !== secondNewDoc);
+        assert.equal(secondNewDoc["@version"], 0);
+        assert(newLink["@rid"] !== newLinkRID);
+        assert.equal(newLink["@version"], 0);
 
         db.loadRecord(secondExistingDoc["@rid"], function(err) {
             assert(err);
