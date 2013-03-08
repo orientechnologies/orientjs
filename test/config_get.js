81b609e21be43185b77b36cbf8db878de8859d80
Federico Fissore <federico@fissore.org>
2013-03-08 15:29:57 +0100

    config_get operation

---
+++ test/config_get.js
@@ -0,0 +1,31 @@
+var assert = require("assert");
+var parser = require("../lib/orientdb/connection/parser");
+var _ = require("lodash");
+
+var orient = require("../lib/orientdb"),
+    Db = orient.Db,
+    Server = orient.Server;
+
+var serverConfig = require("../config/test/serverConfig");
+var dbConfig = require("../config/test/dbConfig");
+
+var server = new Server(serverConfig);
+
+server.connect(function(err) {
+
+    assert(!err, err);
+
+    server.configGet("log.file.level", function(err, value) {
+        assert(!err, err);
+
+        assert(_.isString(value));
+        
+        server.configGet("mvrbtree.loadFactor", function(err, value) {
+            assert(!err, err);
+
+            assert(_.isString(value));
+
+            server.disconnect();
+        })
+    })
+});
