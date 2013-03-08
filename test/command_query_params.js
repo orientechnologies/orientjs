a8cd21a5cfc3563e4cf6107c1795c6fde88a26e8
Federico Fissore <federico@fissore.org>
2013-03-08 13:03:39 +0100

    adding support to query params see #101

---
+++ test/command_query_params.js
@@ -0,0 +1,36 @@
+var assert = require("assert");
+var _ = require("lodash");
+
+var db = require("./test_db.js").db;
+
+db.open(function(err, result) {
+
+    assert(!err, "Error while opening the database: " + err);
+
+    db.query("SELECT FROM OUser where name = :name", { params: { name: "admin" } }, function(err, results) {
+
+        assert(!err, "Error while executing a SELECT command: " + err);
+
+        assert.equal(results.length, 1);
+
+        var admin = results[0];
+        assert(_.isString(admin["@rid"]));
+
+        console.log("Received results: " + JSON.stringify(results));
+
+        db.query("SELECT FROM OUser where name = :name", { fetchPlan: "*:-1", params: { name: "admin" } }, function(err, results) {
+
+            assert(!err, "Error while executing a SELECT command: " + err);
+
+            assert.equal(results.length, 1);
+
+            var admin = results[0];
+            assert(_.isString(admin["@rid"]));
+
+            console.log("Received results: " + JSON.stringify(results));
+
+            db.close();
+        });
+    });
+});
+
