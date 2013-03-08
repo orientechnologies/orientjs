28d7e971df7886776fd225b34de53e2bdb570409
Federico Fissore <federico@fissore.org>
2013-03-08 10:23:14 +0100

    testing Volune commit b6d997cbc89ef6cbe4e452e80bf090709819517e

---
+++ test/db_open_close_twice.js
@@ -0,0 +1,22 @@
+var assert = require("assert");
+var _ = require("lodash");
+
+var db = require("./test_db.js").db;
+
+db.open(function(err) {
+
+    assert(!err, "Error while opening the database: " + err);
+
+    db.close(function(err) {
+
+        assert(!err, "Error while closing the database: " + err);
+
+        db.close(function(err) {
+
+            assert(!err, "Error while closing the database: " + err);
+
+            console.log("Closed database");
+        });
+    });
+});
+
