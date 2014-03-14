dcc1f3e71ffc3f4df735aa3c404f1bf34a92fd5b
Charles Pick <charles.pick@gmail.com>
2014-03-14 10:46:59 +0000

    remove legacy tests

--- test/parser/test_hash_to_sql_sets_0.js
+++
@@ -1,8 +0,0 @@
-var assert = require("assert"),
-    parser = require("../../lib/orientdb/connection/parser");
-
-var sqlsets = parser.hashToSQLSets({});
-
-assert.equal("", sqlsets.sqlsets);
-assert.deepEqual({}, sqlsets.remainingHash);
-
--- test/parser/test_writeString.js
+++
@@ -1,9 +0,0 @@
-var assert = require("assert"),
-    parser = require("../../lib/orientdb/connection/parser");
-
-var string = "This is a test string (èç€) with some special chars";
-var buf = parser.writeString(string);
-
-assert.equal(parser.readString(buf, 0).value, string);
-assert.equal(parser.readString(buf, 0).lengthInBytes, string.length + encodeURIComponent(string).match(/%[89ABab]/g).length);
-
