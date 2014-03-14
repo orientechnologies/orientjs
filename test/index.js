ca2c6348dc6e0f4cc6cb41b0f97996252fb421bf
Charles Pick <charles.pick@gmail.com>
2014-03-14 11:26:50 +0000

    add test entry point and config

---
+++ test/index.js
@@ -0,0 +1,59 @@
+// test bootstrap
+
+var Promise = require('bluebird');
+Promise.longStackTraces();
+
+global.expect = require('expect.js'),
+global.should = require('should');
+
+
+global.TEST_SERVER_CONFIG = require('./test-server.json');
+
+global.LIB = require('../lib/orientdb');
+
+global.TEST_SERVER = new LIB.Server(TEST_SERVER_CONFIG);
+
+
+global.CREATE_TEST_DB = function (context, name) {
+  return TEST_SERVER.exists(name, 'memory')
+  .then(function (exists) {
+    if (exists) {
+      return TEST_SERVER.delete({
+        name: name,
+        storage: 'memory'
+      });
+    }
+    else {
+      return false;
+    }
+  })
+  .then(function () {
+    return TEST_SERVER.create({
+      name: name,
+      type: 'document',
+      storage: 'memory'
+    });
+  })
+  .then(function (db) {
+     context.db = db;
+     return undefined;
+  });
+};
+
+global.DELETE_TEST_DB = function (name) {
+  return TEST_SERVER.exists(name, 'memory')
+  .then(function (exists) {
+    if (exists) {
+      return TEST_SERVER.delete({
+        name: name,
+        storage: 'memory'
+      });
+    }
+    else {
+      return undefined;
+    }
+  })
+  .then(function () {
+    return undefined;
+  });
+};
