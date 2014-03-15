847a02d0e11db18107e80805c216f851a7d654da
Charles Pick <charles.pick@gmail.com>
2014-03-15 23:37:30 +0000

    add tests for parameterized queries

--- test/db/db-test.js
+++ test/db/db-test.js
@@ -33,4 +33,57 @@ describe("Database API", function () {
       }, done).done();
     });
   });
+
+  describe('Db::query()', function () {
+    it('should execute a simple query', function (done) {
+      this.db.query('SELECT * FROM OUser')
+      .then(function (response) {
+        response.results[0].content.length.should.be.above(1);
+        done();
+      }, done).done();
+    });
+    it('should execute a simple query with a limit', function (done) {
+      this.db.query('SELECT * FROM OUser LIMIT 1')
+      .then(function (response) {
+        response.results[0].content.length.should.equal(1);
+        done();
+      }, done).done();
+    });
+    it('should execute a simple query with a limit and a condition', function (done) {
+      this.db.query('SELECT * FROM OUser WHERE name = \'reader\'LIMIT 1')
+      .then(function (response) {
+        response.results[0].content.length.should.equal(1);
+        done();
+      }, done).done();
+    });
+    it('should execute a simple query with a limit and a condition that fails', function (done) {
+      this.db.query('SELECT * FROM OUser WHERE name = \'not_an_existing_user\'LIMIT 1')
+      .then(function (response) {
+        response.results[0].content.length.should.equal(0);
+        done();
+      }, done).done();
+    });
+    it('should execute a numerical parameterized query', function (done) {
+      this.db.query('SELECT * FROM OUser WHERE name = ? LIMIT 1', {
+        params: ['reader']
+      })
+      .then(function (response) {
+        response.results[0].content.length.should.equal(1);
+        response.results[0].content[0].value.name.should.equal('reader');
+        done();
+      }, done).done();
+    });
+    it('should execute a named parameterized query', function (done) {
+      this.db.query('SELECT * FROM OUser WHERE name = :name LIMIT 1', {
+        params: {
+          name: 'writer'
+        }
+      })
+      .then(function (response) {
+        response.results[0].content.length.should.equal(1);
+        response.results[0].content[0].value.name.should.equal('writer');
+        done();
+      }, done).done();
+    });
+  });
 });
