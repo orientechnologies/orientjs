317f3082c13a5a2de96bc98aa1bcb79c434eb1a6
Charles Pick <charles.pick@gmail.com>
2014-03-15 22:33:05 +0000

    make example work with default demo database, also idempotent

--- example/list-classes-create-class.js
+++ example/list-classes-create-class.js
@@ -1,15 +1,20 @@
 var config = require('../test/test-server.json'),
     OrientDB = require('../lib'),
     orientdb = OrientDB(config),
-    db = orientdb.use('testdb_dbapi_workflow1');
+    db = orientdb.use('GratefulDeadConcerts');
 
 
 db.class.list()
 .then(function (results) {
-  console.log(results);
+  console.log('Existing Classes:', results);
   return db.class.create('TestClass');
 })
 .then(function (results) {
-  console.log(results);
+  console.log('Created Class:', results);
+  return db.class.delete('TestClass');
+})
+.then(function (results) {
+  console.log('Deleted Class');
+  process.exit();
 })
 .done();
