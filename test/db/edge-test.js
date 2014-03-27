57c8e4f997b60262bcd7497781ff0fb6f6df78b1
Charles Pick <charles.pick@gmail.com>
2014-03-27 11:39:41 +0000

    add fluent edge api

--- test/db/edge-test.js
+++ test/db/edge-test.js
@@ -1,9 +1,33 @@
 var Class = require('../../lib/db/class');
 
-describe.skip("Database API - Edge", function () {
+describe("Database API - Edge", function () {
   before(function (done) {
     CREATE_TEST_DB(this, 'testdb_dbapi_edge')
-    .then(done, done)
+    .bind(this)
+    .then(function () {
+      return this.db.vertex.create([
+        {
+          name: 'vertex1'
+        },
+        {
+          name: 'vertex2'
+        },
+        {
+          name: 'vertex3'
+        },
+        {
+          name: 'vertex4'
+        }
+      ]);
+    })
+    .reduce(function (list, item) {
+      list[item.name] = item;
+      return list;
+    }, {})
+    .then(function (vertices) {
+      this.vertices = vertices;
+      done();
+    }, done)
     .done();
   });
   after(function (done) {
@@ -12,15 +36,54 @@ describe.skip("Database API - Edge", function () {
     .done();
   });
 
- describe("Db::edge.from().to().create()", function () {
-  it('should create an edge', function (done) {
-    return this.db.edge.from('#5:0').to('#5:1').create()
-    .then(function (response) {
-      console.log(response);
-      done();
-    }, done)
-    .done();
+  describe("Db::edge.from().to().create()", function () {
+    it('should create an edge between individual RIDs', function (done) {
+      return this.db.edge.from(this.vertices.vertex1['@rid']).to(this.vertices.vertex2['@rid']).create()
+      .bind(this)
+      .then(function (edges) {
+        edges.length.should.equal(1);
+        edges[0].out.should.eql(this.vertices.vertex1['@rid']);
+        edges[0].in.should.eql(this.vertices.vertex2['@rid']);
+        expect(edges[0]['@rid']).to.be.undefined;
+        done();
+      }, done)
+      .done();
+    });
+    it('should create an edge between individual RIDs, with some content', function (done) {
+      return this.db.edge.from(this.vertices.vertex1['@rid']).to(this.vertices.vertex3['@rid']).create({
+        key1: 'val1',
+        key2: 'val2'
+      })
+      .bind(this)
+      .then(function (edges) {
+        edges.length.should.equal(1);
+        edges[0].out.should.eql(this.vertices.vertex1['@rid']);
+        edges[0].in.should.eql(this.vertices.vertex3['@rid']);
+        expect(edges[0]['@rid']).to.not.be.undefined;
+        edges[0].key1.should.eql('val1');
+        edges[0].key2.should.eql('val2');
+        done();
+      }, done)
+      .done();
+    });
+    it('should create an edge between lists of RIDs', function (done) {
+      return this.db.edge.from("SELECT FROM OUser WHERE name = 'reader'").to("SELECT FROM ORole").create()
+      .then(function (edges) {
+        edges.length.should.be.above(0);
+        done();
+      }, done)
+      .done();
+    });
   });
- });
 
+  describe("Db::edge.from().to().delete()", function () {
+    it('should delete an edge between individual RIDs', function (done) {
+      return this.db.edge.from(this.vertices.vertex1['@rid']).to(this.vertices.vertex2['@rid']).delete()
+      .then(function (count) {
+        count.should.equal(1);
+        done();
+      }, done)
+      .done();
+    });
+  });
 });
