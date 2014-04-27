901b421590550eeddb8bd57cbddd5ec48cb75a2e
Charles Pick <charles.pick@gmail.com>
2014-04-27 19:14:34 +0100

    add a new RIDBag class

---
+++ test/core/bag-test.js
@@ -0,0 +1,151 @@
+describe("RID Bag", function () {
+  describe('Embedded Bag', function () {
+    before(function () {
+      var self = this;
+      return CREATE_TEST_DB(this, 'testdb_dbapi_rid_bag_embedded')
+      .bind(this)
+      .then(function () {
+        return this.db.class.create('Person', 'V');
+      })
+      .then(function (Person) {
+        this.Person = Person;
+        return this.db.class.create('Knows', 'E');
+      })
+      .then(function (Knows) {
+        this.Knows = Knows;
+        return this.Person.create({
+          name: 'John Smith'
+        });
+      })
+      .then(function (subject) {
+        var limit = 10,
+            i;
+        this.subject = subject;
+        this.people = [];
+        for (i = 0; i < limit; i++) {
+          this.people.push({
+            name: 'Friend ' + i
+          });
+        }
+        return this.Person.create(this.people);
+      })
+      .then(function () {
+        return this.db.edge
+        .from(this.subject['@rid'])
+        .to('SELECT * FROM Person WHERE name LIKE "Friend%"')
+        .create('Knows');
+      });
+    });
+    after(function () {
+      return DELETE_TEST_DB('testdb_dbapi_rid_bag_embedded');
+    });
+
+    beforeEach(function () {
+      return this.db
+      .select()
+      .from(this.subject['@rid'])
+      .fetch({'*': 2})
+      .one()
+      .bind(this)
+      .then(function (record) {
+        this.bag = record.out_Knows;
+      });
+    });
+
+    it('should load a bag', function () {
+      this.bag.should.be.an.instanceOf(LIB.Bag)
+      this.bag.type.should.equal(LIB.Bag.BAG_EMBEDDED);
+      expect(this.bag.uuid).to.equal(null);
+      this.bag.length.should.equal(10);
+    });
+
+    it('should iterate the contents in the bag', function () {
+      var length = this.bag.length,
+          i = 0,
+          item;
+      while((item = this.bag.next())) {
+        item.should.be.an.instanceOf(LIB.RID);
+        i++;
+      }
+      i.should.equal(10);
+    });
+
+    it('should return all the contents of the bag', function () {
+      var contents = this.bag.all();
+      contents.length.should.equal(10);
+      contents.forEach(function (item) {
+        item.should.be.an.instanceOf(LIB.RID);
+      });
+    });
+
+    it('should return the right JSON representation', function () {
+      var json = JSON.stringify(this.bag)
+          decoded = JSON.parse(json);
+      decoded.length.should.equal(10);
+      decoded.forEach(function (item) {
+        (typeof item).should.equal('string');
+      });
+    });
+  });
+
+  describe('Tree Bag', function () {
+    before(function () {
+      var self = this;
+      return CREATE_TEST_DB(this, 'testdb_dbapi_rid_bag_tree', 'plocal')
+      .bind(this)
+      .then(function () {
+        return this.db.class.create('Person', 'V');
+      })
+      .then(function (Person) {
+        this.Person = Person;
+        return this.db.class.create('Knows', 'E');
+      })
+      .then(function (Knows) {
+        this.Knows = Knows;
+        return this.Person.create({
+          name: 'John Smith'
+        });
+      })
+      .then(function (subject) {
+        var limit = 120,
+            i;
+        this.subject = subject;
+        this.people = [];
+        for (i = 0; i < limit; i++) {
+          this.people.push({
+            name: 'Friend ' + i
+          });
+        }
+        return this.Person.create(this.people);
+      })
+      .then(function () {
+        return this.db.edge
+        .from(this.subject['@rid'])
+        .to('SELECT * FROM Person WHERE name LIKE "Friend%"')
+        .create('Knows');
+      });
+    });
+    after(function () {
+      return DELETE_TEST_DB('testdb_dbapi_rid_bag_tree', '');
+    });
+
+    beforeEach(function () {
+      return this.db
+      .select()
+      .from(this.subject['@rid'])
+      .fetch({'*': 2})
+      .one()
+      .bind(this)
+      .then(function (record) {
+        this.bag = record.out_Knows;
+      });
+    });
+
+    it('should load a bag', function () {
+      this.bag.should.be.an.instanceOf(LIB.Bag)
+      this.bag.type.should.equal(LIB.Bag.BAG_TREE);
+      expect(this.bag.uuid).to.equal(null);
+      this.bag.length.should.equal(120);
+    });
+  });
+});
