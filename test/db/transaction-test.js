4dc0fe69ec1d819123901cd54fd9e462ed81ad9e
Charles Pick <charles.pick@gmail.com>
2014-04-28 21:38:13 +0100

    transaction fixes

--- test/db/transaction-test.js
+++ test/db/transaction-test.js
@@ -1,6 +1,6 @@
 var Transaction = require('../../lib/db/transaction');
 
-describe.only("Database API - Transaction", function () {
+describe("Database API - Transaction", function () {
   before(function () {
     return CREATE_TEST_DB(this, 'testdb_dbapi_tx')
     .bind(this)
@@ -18,39 +18,181 @@ describe.only("Database API - Transaction", function () {
       tx.id.should.be.above(0);
     });
   });
-  describe("Db::transaction.create()", function () {
-    beforeEach(function () {
-      this.tx = this.db.begin();
-    });
-    it.only('should create a single record', function () {
-      var completed = false;
-      this.tx.create({
-        '@class': 'TestClass',
-        name: 'item1'
-      })
-      .then(function (record) {
-        record['@rid'].should.be.an.instanceOf(LIB.RID);
-        completed = true;
+
+  describe('Db::commit()', function () {
+    before(function () {
+      return this.db.record.create([
+        {
+          '@class': 'TestClass',
+          name: 'item1'
+        },
+        {
+          '@class': 'TestClass',
+          name: 'item2'
+        }
+      ])
+      .bind(this)
+      .spread(function (first, second) {
+        this.first = first;
+        this.second = second;
       });
-
-      this.tx.create({
-        '@class': 'TestClass',
-        name: 'item2'
-      })
-
+    });
+    it('should perform a single action', function () {
+      this.tx = this.db.begin();
       this.tx.create({
         '@class': 'TestClass',
         name: 'item3'
+      });
+      return this.tx.commit()
+      .then(function (results) {
+        results.created.length.should.equal(1);
+        results.updated.length.should.equal(0);
+        results.deleted.length.should.equal(0);
+      });
+    });
+    it('should perform multiple actions', function () {
+      this.tx = this.db.begin();
+      this.first.wat = 'wat';
+      this.tx
+      .create({
+        '@class': 'TestClass',
+        name: 'item4'
       })
+      .delete(this.second)
+      .update(this.first);
 
       return this.tx.commit()
       .then(function (results) {
-        completed.should.be.true;
+        results.created.length.should.equal(1);
+        results.updated.length.should.equal(1);
+        results.deleted.length.should.equal(1);
       });
+    });
+  });
 
+  describe("Db::transaction.create()", function () {
+    it('should create a single record', function () {
+      this.tx = this.db.begin();
+      return this.tx
+      .create({
+        '@class': 'TestClass',
+        name: 'item1'
+      })
+      .commit()
+      .bind(this)
+      .then(function (results) {
+        results.created.length.should.equal(1);
+        results.updated.length.should.equal(0);
+        results.deleted.length.should.equal(0);
+      });
+    });
+
+    it('should create multiple records', function () {
+      this.tx = this.db.begin();
+      return this.tx
+      .create({
+        '@class': 'TestClass',
+        name: 'item1'
+      })
+      .create({
+        '@class': 'TestClass',
+        name: 'item2'
+      })
+      .create({
+        '@class': 'TestClass',
+        name: 'item3'
+      })
+      .commit()
+      .then(function (results) {
+        results.created.length.should.equal(3);
+        results.updated.length.should.equal(0);
+        results.deleted.length.should.equal(0);
+      });
+    });
+  });
+  describe("Db::transaction.update()", function () {
+    beforeEach(function () {
+      this.tx = this.db.begin();
+      return this.db.record.create([
+        {
+          '@class': 'TestClass',
+          name: 'updateMe1'
+        },
+        {
+          '@class': 'TestClass',
+          name: 'updateMe2'
+        }
+      ])
+      .bind(this)
+      .spread(function (first, second) {
+        this.first = first;
+        this.second = second;
+      });
+    });
+    it('should update a single record', function () {
+      this.first.foo = 'foo';
+      return this.tx
+      .update(this.first)
+      .commit()
+      .then(function (results) {
+        results.created.length.should.equal(0);
+        results.updated.length.should.equal(1);
+        results.deleted.length.should.equal(0);
+      });
+    });
+    it('should update multiple records', function () {
+      this.first.foo = 'foo';
+      this.second.baz = 'baz';
+      return this.tx
+      .update(this.first)
+      .update(this.second)
+      .commit()
+      .then(function (results) {
+        results.created.length.should.equal(0);
+        results.updated.length.should.equal(2);
+        results.deleted.length.should.equal(0);
+      });
     });
   });
   describe("Db::transaction.delete()", function () {
+    beforeEach(function () {
+      this.tx = this.db.begin();
+      return this.db.record.create([
+        {
+          '@class': 'TestClass',
+          name: 'deleteMe1'
+        },
+        {
+          '@class': 'TestClass',
+          name: 'deleteMe2'
+        }
+      ])
+      .bind(this)
+      .spread(function (first, second) {
+        this.first = first;
+        this.second = second;
+      });
+    });
+    it('should delete a single record', function () {
+      return this.tx
+      .delete(this.first)
+      .commit()
+      .then(function (results) {
+        results.created.length.should.equal(0);
+        results.updated.length.should.equal(0);
+        results.deleted.length.should.equal(1);
+      });
+    });
+    it('should delete multiple records', function () {
+      return this.tx
+      .delete(this.first)
+      .delete(this.second)
+      .commit()
+      .then(function (results) {
+        results.created.length.should.equal(0);
+        results.updated.length.should.equal(0);
+        results.deleted.length.should.equal(2);
+      });
+    });
   });
-
 });
