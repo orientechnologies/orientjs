788bd8e09cd3404d304302060a36d34bb08f3d39
Charles Pick <charles.pick@gmail.com>
2014-11-24 15:24:31 +0000

    add db.registerTransformer()

--- test/db/db-test.js
+++ test/db/db-test.js
@@ -1,3 +1,5 @@
+var Promise = require('bluebird');
+
 describe("Database API", function () {
   before(function () {
     return TEST_SERVER.create({
@@ -69,6 +71,85 @@ describe("Database API", function () {
       .then(function (response) {
         response.length.should.equal(1);
         response[0].name.should.equal('writer');
+      });
+    });
+  });
+
+  describe('Db::registerTransformer()', function () {
+    function OUser (data) {
+      if (!(this instanceof OUser)) {
+        return new OUser(data);
+      }
+      var keys = Object.keys(data),
+          length = keys.length,
+          key, i;
+      for (i = 0; i < length; i++) {
+        key = keys[i];
+        this[key] = data[key];
+      }
+    }
+
+    function ORole (data) {
+      if (!(this instanceof ORole)) {
+        return new ORole(data);
+      }
+      var keys = Object.keys(data),
+          length = keys.length,
+          key, i;
+      for (i = 0; i < length; i++) {
+        key = keys[i];
+        this[key] = data[key];
+      }
+    }
+
+
+    before(function () {
+      this.db.registerTransformer('OUser', OUser);
+      this.db.registerTransformer('ORole', ORole);
+    });
+
+    it('should register a transformation function for a class', function () {
+      return Promise.all([
+        this.db.select().from('OUser').all(),
+        this.db.select().from('ORole').all()
+      ])
+      .spread(function (users, roles) {
+        users.length.should.be.above(0);
+        users.forEach(function (user) {
+          user.should.be.an.instanceOf(OUser);
+        });
+        roles.length.should.be.above(0);
+        roles.forEach(function (role) {
+          role.should.be.an.instanceOf(ORole);
+        });
+      });
+    });
+
+    it('should transform documents even when they are nested', function () {
+      return this.db.select().from('OUser').fetch({roles: 1}).all()
+      .then(function (users) {
+        users.length.should.be.above(0);
+        users.forEach(function (user) {
+          user.should.be.an.instanceOf(OUser);
+          user.roles.length.should.be.above(0);
+          user.roles.forEach(function (role) {
+            role.should.be.an.instanceOf(ORole);
+          });
+        });
+      });
+    });
+
+    it('should still allow scalars', function () {
+      return this.db.select().from('OUser').limit(1).scalar()
+      .then(function (result) {
+        result.should.equal('admin');
+      });
+    });
+
+    it('should not transform when individual columns are selected', function () {
+      return this.db.select('name, status').from('OUser').limit(1).one()
+      .then(function (result) {
+        result.should.not.be.an.instanceOf(OUser);
       });
     });
   });
