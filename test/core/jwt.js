fef3482d413b5e71ef1a2968f229bb1dca5848a3
Charles Pick <charles.pick@gmail.com>
2014-12-17 00:55:10 +0000

    disable JWT tests on protocol versions prior to 28

--- test/core/jwt.js
+++ test/core/jwt.js
@@ -1,7 +1,14 @@
 var Bluebird = require('bluebird');
-
 describe('JWT', function () {
-  var server;
+  var hasProtocolSupport = false,
+      server = null;
+  function ifSupportedIt (text, fn) {
+    it(text, function () {
+      if (hasProtocolSupport) {
+        return fn();
+      }
+    });
+  }
   before(function () {
     server = new LIB.Server({
       host: TEST_SERVER_CONFIG.host,
@@ -11,7 +18,11 @@ describe('JWT', function () {
       transport: 'binary',
       useToken: true
     });
-    return CREATE_TEST_DB(this, 'testdb_jwt');
+    return CREATE_TEST_DB(this, 'testdb_jwt')
+    .bind(this)
+    .then(function () {
+      hasProtocolSupport = this.db.server.transport.connection.protocolVersion >= 28;
+    });
   });
   after(function () {
     return DELETE_TEST_DB('testdb_jwt');
@@ -24,11 +35,11 @@ describe('JWT', function () {
         dbs = items;
       });
     });
-    it('should connect to the server and get a token', function () {
+    ifSupportedIt('should connect to the server and get a token', function () {
       server.token.should.be.an.instanceOf(Buffer);
       server.token.length.should.be.above(0);
     });
-    it('should retrieve a list of databases', function () {
+    ifSupportedIt('should retrieve a list of databases', function () {
       dbs.length.should.be.above(0);
     });
   });
@@ -42,14 +53,14 @@ describe('JWT', function () {
       });
       return db.open();
     })
-    it('should open a database and get a token', function () {
+    ifSupportedIt('should open a database and get a token', function () {
       db.token.should.be.an.instanceOf(Buffer);
       db.token.length.should.be.above(0);
     });
-    it('should return a different token from the server token', function () {
+    ifSupportedIt('should return a different token from the server token', function () {
       db.token.toString().should.not.equal(server.token.toString());
     });
-    it('should execute commands using the token', function () {
+    ifSupportedIt('should execute commands using the token', function () {
       return db.select().from('OUser').all()
       .then(function (users) {
         users.length.should.be.above(0);
@@ -59,23 +70,25 @@ describe('JWT', function () {
   describe('JWT Database::query()', function () {
     var db, admin, reader, writer;
     before(function () {
-      db = server.use('testdb_jwt');
-      return Bluebird.all([
-        server.use({name: 'testdb_jwt', username: 'admin', password: 'admin'}).open(),
-        server.use({name: 'testdb_jwt', username: 'reader', password: 'reader'}).open(),
-        server.use({name: 'testdb_jwt', username: 'writer', password: 'writer'}).open()
-      ])
-      .then(function (items) {
-        admin = items[0].token;
-        reader = items[1].token;
-        writer = items[2].token;
+      if (hasProtocolSupport) {
+        db = server.use('testdb_jwt');
+        return Bluebird.all([
+          server.use({name: 'testdb_jwt', username: 'admin', password: 'admin'}).open(),
+          server.use({name: 'testdb_jwt', username: 'reader', password: 'reader'}).open(),
+          server.use({name: 'testdb_jwt', username: 'writer', password: 'writer'}).open()
+        ])
+        .then(function (items) {
+          admin = items[0].token;
+          reader = items[1].token;
+          writer = items[2].token;
 
-        admin.toString().should.not.equal(reader.toString());
-        admin.toString().should.not.equal(writer.toString());
-        writer.toString().should.not.equal(reader.toString());
-      });
+          admin.toString().should.not.equal(reader.toString());
+          admin.toString().should.not.equal(writer.toString());
+          writer.toString().should.not.equal(reader.toString());
+        });
+      }
     });
-    it('should not allow the reader to create a vertex', function () {
+    ifSupportedIt('should not allow the reader to create a vertex', function () {
       return db.create('VERTEX', 'V').set({foo: 'bar'}).token(reader).one()
       .then(function () {
         throw new Error('No, this should not happen');
@@ -84,44 +97,44 @@ describe('JWT', function () {
         /permission/i.test(err.message).should.be.true;
       });
     });
-    it('should allow the reader to read from a class', function () {
+    ifSupportedIt('should allow the reader to read from a class', function () {
       return db.select().from('OUser').token(reader).all()
       .then(function (users) {
         users.length.should.be.above(0);
       });
     });
-    it('should allow the writer to create a vertex', function () {
+    ifSupportedIt('should allow the writer to create a vertex', function () {
       return db.create('VERTEX', 'V').set({foo: 'bar'}).token(writer).one()
       .then(function (item) {
         item.foo.should.equal('bar');
       });
     });
-    it('should allow the writer to read from a class', function () {
+    ifSupportedIt('should allow the writer to read from a class', function () {
       return db.select().from('OUser').token(writer).all()
       .then(function (users) {
         users.length.should.be.above(0);
       });
     });
-    it('should allow the admin to create a vertex', function () {
+    ifSupportedIt('should allow the admin to create a vertex', function () {
       return db.create('VERTEX', 'V').set({foo: 'bar'}).token(admin).one()
       .then(function (item) {
         item.foo.should.equal('bar');
       });
     });
-    it('should allow the admin to read from a class', function () {
+    ifSupportedIt('should allow the admin to read from a class', function () {
       return db.select().from('OUser').token(admin).all()
       .then(function (users) {
         users.length.should.be.above(0);
       });
     });
 
-    it('should allow the default user to create a vertex', function () {
+    ifSupportedIt('should allow the default user to create a vertex', function () {
       return db.create('VERTEX', 'V').set({foo: 'bar'}).one()
       .then(function (item) {
         item.foo.should.equal('bar');
       });
     });
-    it('should allow the default user to read from a class', function () {
+    ifSupportedIt('should allow the default user to read from a class', function () {
       return db.select().from('OUser').all()
       .then(function (users) {
         users.length.should.be.above(0);
