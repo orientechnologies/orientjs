5e76a16937033feaa87225c6d920ee18d975a2a4
Dario Marcelino <dario@appscot.com>
2015-01-17 00:56:19 +0000

    Issue #203, query serialisation: add support for date

--- test/core/utils.js
+++ test/core/utils.js
@@ -9,4 +9,8 @@ describe('utils.prepare', function () {
   it("should prepare SQL statements with parameters", function () {
     utils.prepare("select from index:foo where key = :key", {key: 123}).should.equal("select from index:foo where key = 123");
   });
+  it("should prepare SQL statements with date parameters", function () {
+    var date = new Date();
+    utils.prepare("select from index:foo where date = :date", {date: date}).should.equal("select from index:foo where date = " + date.getTime());
+  });
 });
