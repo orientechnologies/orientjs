0f8ae017560f61687927c7d6659b6e4180a0e3f1
Charles Pick <charles.pick@gmail.com>
2015-02-23 16:22:17 +0000

    add helper that determines whether the given statement requires wrapping in parentheses or not

--- test/core/utils.js
+++ test/core/utils.js
@@ -20,3 +20,38 @@ describe('utils.prepare', function () {
     utils.prepare("select from index:foo where date = :date", {date: date}).should.equal("select from index:foo where date = date(\"2015-01-05 22:07:05\", \"yyyy-MM-dd HH:mm:ss\", \"UTC\")");
   });
 });
+
+
+describe('utils.requiresParens()', function () {
+  it('should not require parentheses for string values', function () {
+    utils.requiresParens('"foo"').should.be.false;
+  });
+  it('should not require parentheses for integer values', function () {
+    utils.requiresParens('2134').should.be.false;
+  });
+  it('should not require parentheses for function calls', function () {
+    utils.requiresParens('foo("hello", "world")').should.be.false;
+  });
+  it('should require parentheses for compound expressions with strings', function () {
+    utils.requiresParens('"foo" AND "bar"').should.be.true;
+  });
+  it('should not require parentheses for pre-wrapped compound expressions with strings', function () {
+    utils.requiresParens('("foo" AND "bar")').should.be.false;
+  });
+  it('should require parentheses for compound call expressions', function () {
+    utils.requiresParens('foo("hello", "world") AND wat').should.be.true;
+    utils.requiresParens('foo("hello", "world") AND bar("wat")').should.be.true;
+  });
+  it('should not require parentheses for deeply nested function calls', function () {
+    utils.requiresParens("foo(1,2,3, bar(4,5,6))").should.be.false;
+  });
+  it('should require parentheses for separately parenthesized expressions', function () {
+    utils.requiresParens("(foo(1,2,3) AND test) AND (a > 2)").should.be.true;
+  });
+  it('should not require parentheses for strings with parenthesized expressions', function () {
+    utils.requiresParens("'foo (a b c) wat'").should.be.false;
+  });
+  it('should ignore leading and trailing whitespace', function () {
+    utils.requiresParens('    ("foo" AND "bar")   ').should.be.false;
+  });
+});
