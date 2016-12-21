0f284924698f1383962e85a3e1ca719ef6fc7d8a
maggiolo00 <maggiolo00@gmail.com>
2016-12-21 12:44:54 +0100

    Changed test from e.type to e.message

--- test/transport/binary/binary-transport-test.js
+++ test/transport/binary/binary-transport-test.js
@@ -3,12 +3,12 @@ describe("Binary Transport", function () {
   describe('BinaryTransport::connect()', function () {
     it("should negotiate a connection", function () {
       return BINARY_TEST_SERVER.transport.connect()
-      .then(function (server) {
-        server.sessionId.should.be.above(-1);
-      });
+        .then(function (server) {
+          server.sessionId.should.be.above(-1);
+        });
     });
   });
-  describe('BinaryTransport::send()', function  () {
+  describe('BinaryTransport::send()', function () {
     it("should handle errors correctly", function () {
       return BINARY_TEST_SERVER.transport.send('db-open', {
         name: 'not_an_existing_database',
@@ -16,13 +16,13 @@ describe("Binary Transport", function () {
         username: 'admin',
         password: 'admin'
       })
-      .then(function (response) {
-        throw new Error('Should Not Happen!');
-      })
-      .catch(errors.Request, function (e) {
-        e.type.should.equal('com.orientechnologies.orient.core.exception.OConfigurationException');
-        return true;
-      });
+        .then(function (response) {
+          throw new Error('Should Not Happen!');
+        })
+        .catch(errors.Request, function (e) {
+          e.message.should.equal('Cannot open database \'not_an_existing_database\'');
+          return true;
+        });
     })
   });
 });
