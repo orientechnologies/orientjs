2ad1aa8842835114e97b3ae5923cfa67359dc8c4
maggiolo00 <maggiolo00@gmail.com>
2018-05-11 16:52:30 +0200

    Removed Oservable. Used Stream instead

--- test/database/database-live-query-test.js
+++ test/database/database-live-query-test.js
@@ -1,244 +1,189 @@
-var Promise = require('bluebird');
-var Errors = require('../../lib/errors');
-describe("ODatabase API - Live Query ", function () {
-
-
-  before(CAN_RUN(37,function () {
-    return CREATE_DB("test_session_api_query")
-      .bind(this)
-      .then(function () {
-        return TEST_CLIENT.open({name: "test_session_api_query"});
-      })
-      .then(function (session) {
-        this.session = session;
-        return this.session.class.create('Test', 'V');
-      })
-      .then(function (item) {
-        this.class = item;
-        return this.class.property.create([
-          {
-            name: 'name',
-            type: 'String'
-          },
-          {
-            name: 'creation',
-            type: 'DateTime'
-          }
-        ]);
-      })
-      .then(function () {
-        return this.class.create([
-          {
-            name: 'a',
-            creation: '2001-01-01 00:00:01'
-          },
-          {
-            name: 'b',
-            creation: '2001-01-02 12:00:01'
-          },
-          {
-            name: 'c',
-            creation: '2009-01-01 00:12:01'
-          },
-          {
-            name: 'd',
-            creation: '2014-09-01 00:01:01'
-          },
-          {
-            name: 'e',
-            creation: '2014-09-01 00:24:01'
-          }
-        ])
-      });
-  }));
-  after(function () {
-    return DELETE_TEST_DB('test_session_api_query');
-  });
-
-  it('should subscribe/unSubscribe live query', function (done) {
-
-    var observable = this.session.liveQuery("LIVE SELECT FROM Test");
-    observable.subscribe(function (live) {
-      throw new Error('Should never happen!');
-    }, function (err) {
-      throw new Error('Should never happen!');
-    }, function () {
-      done();
-    });
-
-    observable.unSubscribe();
-
-
-  });
-
-  it('should trigger live query create', function (done) {
-
-    var TOTAL = 2;
-    var count = 0;
-
-
-    var observable = this.session.liveQuery("LIVE SELECT FROM Test");
-    observable.subscribe(function (live) {
-      count++;
-      live.operation.should.eql(1);
-      live.data.name.should.eql('a');
-
-      if (count === TOTAL) {
-        observable.unSubscribe();
-      }
-    }, function (err) {
-      throw new Error('Should never happen!');
-    }, function () {
-      done();
-    });
-
-
-    var self = this;
-    setTimeout(function () {
-      var promises = [];
-      for (var i = 0; i < TOTAL; i++) {
-        promises.push(self.session.create("VERTEX", "Test")
-          .set(
+var Promise = require("bluebird");
+var Errors = require("../../lib/errors");
+describe("ODatabase API - Live Query ", function() {
+  before(
+    CAN_RUN(37, function() {
+      return CREATE_DB("test_session_api_query")
+        .bind(this)
+        .then(function() {
+          return TEST_CLIENT.open({ name: "test_session_api_query" });
+        })
+        .then(function(session) {
+          this.session = session;
+          return this.session.class.create("Test", "V");
+        })
+        .then(function(item) {
+          this.class = item;
+          return this.class.property.create([
             {
-              name: 'a',
-              creation: '2001-01-01 00:00:01'
-            })
-          .one())
-      }
-      Promise.all(promises).then(function (rec) {
-      })
-    }, 1000);
-
+              name: "name",
+              type: "String"
+            },
+            {
+              name: "creation",
+              type: "DateTime"
+            }
+          ]);
+        })
+        .then(function() {
+          return this.class.create([
+            {
+              name: "a",
+              creation: "2001-01-01 00:00:01"
+            },
+            {
+              name: "b",
+              creation: "2001-01-02 12:00:01"
+            },
+            {
+              name: "c",
+              creation: "2009-01-01 00:12:01"
+            },
+            {
+              name: "d",
+              creation: "2014-09-01 00:01:01"
+            },
+            {
+              name: "e",
+              creation: "2014-09-01 00:24:01"
+            }
+          ]);
+        });
+    })
+  );
+  after(function() {
+    return DELETE_TEST_DB("test_session_api_query");
   });
 
-  it('should trigger live query create with more subscribers', function (done) {
+  it("should subscribe/unSubscribe live query", function(done) {
+    let live = this.session
+      .liveQuery("LIVE SELECT FROM Test")
+      .on("data", () => {
+        throw new Error("Should never happen!");
+      })
+      .on("error", err => {
+        throw new Error("Should never happen!");
+      })
+      .on("end", () => {
+        done();
+      });
 
+    setTimeout(() => {
+      live.unsubscribe();
+    }, 500);
+  });
+
+  it("should trigger live query create", function(done) {
     var TOTAL = 2;
     var count = 0;
-    var isDone = false;
-
-    var observable = this.session.liveQuery("LIVE SELECT FROM Test");
-
-    var observer = {
-      next: function (live) {
+    let liveQuery = this.session
+      .liveQuery("LIVE SELECT FROM Test")
+      .on("data", live => {
         count++;
         live.operation.should.eql(1);
-        live.data.name.should.eql('a');
-
-        if (count === TOTAL + 2) {
-          observable.unSubscribe();
+        live.data.name.should.eql("a");
+        if (count === TOTAL) {
+          liveQuery.unsubscribe();
         }
-      },
-      error: function (err) {
-        throw new Error('Should never happen!');
-      },
-      complete: function () {
-
-        if (!isDone) {
-          isDone = true;
-          done();
-        }
-      }
-    };
-    observable.subscribe(observer);
-    observable.subscribe(observer);
-
+      })
+      .on("error", () => {
+        throw new Error("Should never happen!");
+      })
+      .on("end", () => {
+        done();
+      });
     var self = this;
-    setTimeout(function () {
+    setTimeout(function() {
       var promises = [];
       for (var i = 0; i < TOTAL; i++) {
-        promises.push(self.session.create("VERTEX", "Test")
-          .set(
-            {
-              name: 'a',
-              creation: '2001-01-01 00:00:01'
+        promises.push(
+          self.session
+            .create("VERTEX", "Test")
+            .set({
+              name: "a",
+              creation: "2001-01-01 00:00:01"
             })
-          .one())
+            .one()
+        );
       }
-      Promise.all(promises).then(function (rec) {
-      })
+      Promise.all(promises).then(function(rec) {});
     }, 1000);
-
   });
 
-  it('should trigger live query updated', function (done) {
-
-
-    this.session.create("VERTEX", "Test")
-      .set(
-        {
-          name: 'a',
-          creation: '2001-01-01 00:00:01'
-        })
+  it("should trigger live query updated", function(done) {
+    this.session
+      .create("VERTEX", "Test")
+      .set({
+        name: "a",
+        creation: "2001-01-01 00:00:01"
+      })
       .one()
-      .then((record) => {
-        var observable = this.session.liveQuery("LIVE SELECT FROM Test");
-        observable.subscribe(function (live) {
-          live.operation.should.eql(2);
-          live.data.name.should.eql('b');
-          live.before.name.should.eql('a');
-          observable.unSubscribe();
-        }, function (err) {
-          throw new Error('Should never happen!');
-        }, function () {
-          done();
-        });
+      .then(record => {
+        let liveQuery = this.session
+          .liveQuery("LIVE SELECT FROM Test")
+          .on("data", live => {
+            live.operation.should.eql(2);
+            live.data.name.should.eql("b");
+            live.before.name.should.eql("a");
+            liveQuery.unsubscribe();
+          })
+          .on("error", () => {
+            throw new Error("Should never happen!");
+          })
+          .on("end", () => {
+            done();
+          });
 
         var self = this;
-        setTimeout(function () {
-
+        setTimeout(function() {
           record.name = "b";
-          self.session.record.update(record)
-            .then((updated) => {
-            })
+          self.session.record.update(record).then(updated => {});
         }, 1000);
       });
-
   });
 
-  it('should trigger live query deleted', function (done) {
-
-    this.session.create("VERTEX", "Test")
-      .set(
-        {
-          name: 'a',
-          creation: '2001-01-01 00:00:01'
-        })
+  it("should trigger live query deleted", function(done) {
+    this.session
+      .create("VERTEX", "Test")
+      .set({
+        name: "a",
+        creation: "2001-01-01 00:00:01"
+      })
       .one()
-      .then((record) => {
-        var observable = this.session.liveQuery("LIVE SELECT FROM Test");
-        observable.subscribe(function (live) {
-          live.operation.should.eql(3);
-          live.data.name.should.eql('a');
-          observable.unSubscribe();
-        }, function (err) {
-          throw new Error('Should never happen!');
-        }, function () {
-          done();
-        });
+      .then(record => {
+        let liveQuery = this.session
+          .liveQuery("LIVE SELECT FROM Test")
+          .on("data", live => {
+            live.operation.should.eql(3);
+            live.data.name.should.eql("a");
+            liveQuery.unsubscribe();
+          })
+          .on("error", () => {
+            throw new Error("Should never happen!");
+          })
+          .on("end", () => {
+            done();
+          });
 
         var self = this;
-        setTimeout(function () {
-
-          self.session.record.delete(record)
-            .then((updated) => {
-            })
+        setTimeout(function() {
+          self.session.record.delete(record).then(updated => {});
         }, 1000);
       });
-
   });
 
-  it('should trigger live query error', function (done) {
-
-    this.session.liveQuery("LIVE SELECT FROM Test2")
-      .subscribe(function (live) {
-        throw new Error('Should never happen!');
-      }, function (err) {
+  it("should trigger live query error", function(done) {
+    this.session
+      .liveQuery("LIVE SELECT FROM Test2")
+      .on("data", () => {
+        throw new Error("Should never happen!");
+      })
+      .on("error", err => {
         err.should.be.an.instanceOf(Errors.RequestError);
         done();
-      }, function () {
-        throw new Error('Should never happen!');
+      })
+      .on("end", () => {
+        throw new Error("Should never happen!");
       });
   });
-
 });
--- test/database/database-query-streaming-test.js
+++ test/database/database-query-streaming-test.js
@@ -1,94 +1,62 @@
-var Query = require('../../lib/client/database/database-query');
+var Query = require("../../lib/client/database/database-query");
 
-describe("ODatabase API - Query", function () {
-
-  before(CAN_RUN(37, function () {
-    return CREATE_DB("test_session_streaming")
-      .then(() => {
-        return TEST_CLIENT.open({name: "test_session_streaming"});
-      })
-      .then((db) => {
-        this.db = db;
-      })
-  }));
-  after(function () {
+describe("ODatabase API - Query", function() {
+  before(
+    CAN_RUN(37, function() {
+      return CREATE_DB("test_session_streaming")
+        .then(() => {
+          return TEST_CLIENT.open({ name: "test_session_streaming" });
+        })
+        .then(db => {
+          this.db = db;
+        });
+    })
+  );
+  after(function() {
     return DROP_DB("test_session_streaming");
   });
 
-  beforeEach(function () {
+  beforeEach(function() {
     this.query = new Query(this.db);
   });
 
-
-  describe('ODatabase::query::subscribe()', function () {
-
-
-    it('should execute a query with page size custom', function (done) {
-      return this.db.query('select from OUSer', {
-        pageSize: 1
-      })
-        .reduce(function (acc, val) {
-          acc.push(val);
-          return acc;
-        }, [])
-        .subscribe(function (response) {
-          response.length.should.be.eql(3);
+  describe("ODatabase::query::subscribe()", function() {
+    it("should execute a query with page size custom", function(done) {
+      var results = [];
+      return this.db
+        .query("select from OUSer", {
+          pageSize: 1
+        })
+        .on("data", record => {
+          results.push(record);
+        })
+        .on("end", () => {
+          results.length.should.be.eql(3);
           done();
         });
     });
-
-    it('should execute a query with multiple subscribers', function (done) {
-      var observable = this.db.query('select from OUSer');
-
-      var count = 0;
-      var completed = false;
-
-      var queryId = null;
-      var observer = {
-        next: function (user) {
-          count++;
-          user.should.have.property('name');
-          user.should.have.property('@class');
-          user["@class"].should.be.eql('OUser');
-          if (queryId == null) {
-            queryId = observable.queryId;
-          }
-        },
-        error: function () {
-          throw  new Error("It should never happen!");
-        },
-        complete: function () {
-
-          if (!completed) {
-
-
-            count.should.be.eql(6);
-            completed = true;
-            done();
-          }
-        }
-      }
-      observable.subscribe(observer);
-      observable.subscribe(observer);
-    });
   });
 
-
-  describe('ODatabase::queryBuilder::stream()', function () {
-    it('should return one record with stream and limit', function (done) {
+  describe("ODatabase::queryBuilder::stream()", function() {
+    it("should return one record with stream and limit", function(done) {
       var user;
       var size = 0;
-      this.query.select().from('OUser').limit(1).stream().subscribe((response) => {
-        user = response;
-        size++;
-      }, (err) => {
-
-      }, () => {
-        size.should.be.eql(1);
-        Array.isArray(user).should.be.false;
-        user.should.have.property('name');
-        done();
-      })
+      this.query
+        .select()
+        .from("OUser")
+        .limit(1)
+        .stream()
+        .on("data", response => {
+          user = response;
+          size++;
+        })
+        .on("error", err => {})
+        .on("end", () => {
+          size.should.be.eql(1);
+          Array.isArray(user).should.be.false;
+          user.should.have.property("name");
+          done();
+        });
     });
   });
 });
