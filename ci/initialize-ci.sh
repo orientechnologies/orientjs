47623e7f410659bafcb2505fc856b8279cfaf5c0
Tglman <tglman@tglman.com>
2023-10-25 14:29:31 +0100

    chore: update ci to the last OrientDB version

--- ci/initialize-ci.sh
+++ ci/initialize-ci.sh
@@ -3,7 +3,7 @@
 PARENT_DIR=$(dirname $(cd "$(dirname "$0")"; pwd))
 CI_DIR="$PARENT_DIR/ci/environment"
 
-ODB_VERSION=${1:-"3.2.21"}
+ODB_VERSION=${1:-"3.2.24"}
 ODB_DIR="${CI_DIR}/orientdb-community-${ODB_VERSION}"
 ODB_LAUNCHER="${ODB_DIR}/bin/server.sh"
 
